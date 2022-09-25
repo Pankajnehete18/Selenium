73e18d25218f4e5d54afe1cefd62751d371f5a14
Pankajnehete18 <pankajnehete227@gmail.com>
2022-09-25 16:29:04 +0530

    first Trial

---
+++ src/test/java/ExtentReportGenerate/ExtentReportGenerateDemo.java
@@ -0,0 +1,46 @@
+package ExtentReportGenerate;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportGenerateDemo {
+
+	ExtentReports extent;
+
+	@BeforeTest
+	public void extentReport() {
+
+		String path = System.getProperty("user.dir") + "\\reports.index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester Name", "Pankaj Nehete");
+
+	}
+
+	    @Test
+	   public void extentReportDemo() {
+
+		ExtentTest test=extent.createTest("extentReportDemo");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result do not match");
+		extent.flush();
+
+	}
+
+}
